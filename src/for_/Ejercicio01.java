91c86ce84ff1d31aea1768c4f0989edf7b9ec01a
gonzalolopez77 <gonzalo.lopez@iesnervion.es>
2025-11-06 14:41:03 +0100

    a

---
+++ src/for_/Ejercicio01.java
@@ -0,0 +1,21 @@
+package for_;
+
+import java.util.Scanner;
+
+public class Ejercicio01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Crea el scanner
+		Scanner sc = new Scanner(System.in);
+		
+		//
+		
+		
+		//Cierra el scanner
+		sc.close();
+		
+	}
+
+}
