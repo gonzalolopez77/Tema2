d5abebd543383368dd2fe1f9294de951c141b0ae
gonzalolopez77 <gonzalo.lopez@iesnervion.es>
2025-11-10 09:10:30 +0100

    a

--- src/for_/Ejercicio01.java
+++ src/for_/Ejercicio01.java
@@ -10,9 +10,18 @@ public class Ejercicio01 {
 		//Crea el scanner
 		Scanner sc = new Scanner(System.in);
 		
-		//
-		
-		
+		//Variable num
+		int num;
+				
+		// Le pedimos un número al usuario que se almacena en num
+		System.out.println("Introduce un numero:");
+		num = sc.nextInt();
+				
+		// for: desde 1 hasta num de 1 en 1
+		for(int cont = 1; cont<= num; cont++) {
+			System.out.println(cont);
+		}
+				
 		//Cierra el scanner
 		sc.close();
 		
